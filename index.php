497c54a38531dca655d32c243dd2d333b152ba3f
debarones <de_barones@protonmail.com>
2020-11-22 15:14:17 +0100

    win

--- index.php
+++ index.php
@@ -19,7 +19,15 @@
   </header>
   <div class="wrapper">
     <article class="about">
-      <h1>DP 10008</h1>
+      <h1>DP 10008 | 505</h1>
+      <div class="ic_txt">
+        <div class="ic"><img src="images/truck_c.png" alt="" width="52" height="32"></div>
+        <div class="txt">SCANIA F320</div>
+      </div></br>
+      <div class="ic_txt">
+        <div class="ic"><img src="images/jakk.png" alt="" width="42" height="32"></div>
+        <div class="txt">320</div>
+      </div></br>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean condimentum condimentum efficitur. Nulla sagittis lorem ante, id bibendum libero pharetra quis. Praesent sit amet metus neque. Nullam ut libero elit. Quisque eu auctor purus.
         Phasellus in purus nulla. Vivamus aliquet leo leo, non ullamcorper arcu finibus id. Vestibulum fermentum neque sed rutrum ultrices. Maecenas porttitor bibendum neque, non efficitur dui dapibus quis. Donec vehicula tortor nec eros bibendum
         consequat. Maecenas dictum a odio posuere tincidunt. Donec imperdiet massa non purus elementum luctus. </p>
