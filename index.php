5db0ff5a863b56b54ae1e26a74287a2f167b83af
debarones <74858577+debarones@users.noreply.github.com>
2020-11-22 12:44:15 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>Cargotron trucks & equipments</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+  <header class="header">
+    <div class="img-logo">
+      <img src="images/logo.png" alt="" width="330" height="70">
+    </div>
+    <div class="img-logo1">
+      <img src="images/logo1.png" alt="" width="72" height="72">
+    </div>
+  </header>
+  <div class="wrapper">
+    <article class="about">
+      <h1>DP 10008</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean condimentum condimentum efficitur. Nulla sagittis lorem ante, id bibendum libero pharetra quis. Praesent sit amet metus neque. Nullam ut libero elit. Quisque eu auctor purus.
+        Phasellus in purus nulla. Vivamus aliquet leo leo, non ullamcorper arcu finibus id. Vestibulum fermentum neque sed rutrum ultrices. Maecenas porttitor bibendum neque, non efficitur dui dapibus quis. Donec vehicula tortor nec eros bibendum
+        consequat. Maecenas dictum a odio posuere tincidunt. Donec imperdiet massa non purus elementum luctus. </p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean condimentum condimentum efficitur. Nulla sagittis lorem ante, id bibendum libero pharetra quis. Praesent sit amet metus neque. Nullam ut libero elit. Quisque eu auctor purus.
+        Phasellus in purus nulla. Vivamus aliquet leo leo, non ullamcorper arcu finibus id. Vestibulum fermentum neque sed rutrum ultrices. Maecenas porttitor bibendum neque, non efficitur dui dapibus quis. Donec vehicula tortor nec eros bibendum
+        consequat. Maecenas dictum a odio posuere tincidunt. Donec imperdiet massa non purus elementum luctus. </p>
+    </article>
+  </div>
+</body>
+
+</html>
